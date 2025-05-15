5ce216d3ed2798f814722935c1069a97d6be0376
Dennis Mitchell <Dennis.C.Mitchell@ct.gov>
2025-05-15 12:28:00 -0400

    Create Singleton for holding Pre-Parsed Templates; Create Controller for converting data

---
+++ Fhir.Converter.Api/Controllers/ConverterController.cs
@@ -0,0 +1,89 @@
+﻿using DotLiquid;
+using DotLiquid.Exceptions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Health.Fhir.Liquid.Converter;
+using Microsoft.Health.Fhir.Liquid.Converter.DotLiquids;
+using Microsoft.Health.Fhir.Liquid.Converter.Exceptions;
+using Microsoft.Health.Fhir.Liquid.Converter.Models;
+using Microsoft.Health.Fhir.Liquid.Converter.Processors;
+using Namotion.Reflection;
+using System.Globalization;
+
+namespace Fhir.Converter.Api.Controllers
+{
+    public class ConverterController(CachedTemplateProviderDictionary cache) : Controller
+    {
+        private readonly CachedTemplateProviderDictionary cache = cache;
+
+        private static readonly ProcessorSettings processorSettings = new() { };
+
+
+        [HttpGet("hl7v2-to-fhir/{template}")]
+        public IActionResult Hl7v2ToFhir(string template, [FromBody] string payload,
+            [FromServices] ILogger<Hl7v2Processor> logger)
+        {
+            ITemplateProvider templateProvider = cache.TemplateProviders[
+                CachedTemplateProviderDictionary.HL7V2_TEMPLATE_PROVIDER_KEY];
+
+            Hl7v2Processor processor = new(processorSettings, logger);
+
+            try
+            {
+                string result = processor.Convert(payload, template, templateProvider);
+                return new ContentResult { Content = result };
+            }
+            catch (TemplateLoadException ex)
+            {
+                return new BadRequestObjectResult($"Could not process hl7v2-to-fhir/{{template}}: {ex.Message}");
+            }
+
+        }
+
+
+        [HttpGet("ccda-to-fhir/{template}")]
+        public IActionResult CcdaToFhir(string template, [FromBody] string payload,
+            [FromServices] ILogger<CcdaProcessor> logger)
+        {
+            ITemplateProvider templateProvider = cache.TemplateProviders[
+                CachedTemplateProviderDictionary.HL7V2_TEMPLATE_PROVIDER_KEY];
+
+            CcdaProcessor processor = new(processorSettings, logger);
+
+            try
+            {
+                string result = processor.Convert(payload, template, templateProvider);
+                return new ContentResult { Content = result };
+            }
+            catch (TemplateLoadException ex)
+            {
+                return new BadRequestObjectResult($"Could not process ccda-to-fhir/{{template}}: {ex.Message}");
+            }
+
+        }
+
+
+        [HttpGet("json-to-fhir/{template}")]
+        public IActionResult JsonToFhir(string template, [FromBody] string payload,
+            [FromServices] ILogger<CcdaProcessor> logger)
+        {
+            ITemplateProvider templateProvider = cache.TemplateProviders[
+                CachedTemplateProviderDictionary.HL7V2_TEMPLATE_PROVIDER_KEY];
+
+            CcdaProcessor processor = new(processorSettings, logger);
+
+            try
+            {
+                string result = processor.Convert(payload, template, templateProvider);
+                return new ContentResult { Content = result };
+            }
+            catch (TemplateLoadException ex)
+            {
+                return new BadRequestObjectResult($"Could not process json-to-fhir/{{template}}: {ex.Message}");
+            }
+
+        }
+
+
+    }
+}
